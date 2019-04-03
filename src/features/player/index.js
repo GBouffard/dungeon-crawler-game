63033f7c47da8a9cf2172cb8c7c6f925d561c3e6
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-03 15:34:53 +0200

    Add the player sprite, style and connect Player with mapStateToProps

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import walkSprite from "./player_walk.png";
 
-const Player = () => <div>Player</div>;
+const Player = props => (
+  <div
+    style={{
+      position: "absolute",
+      top: props.position[1],
+      left: props.position[0],
+      backgroundImage: `url('${walkSprite}')`,
+      backgroundPosition: "0 0",
+      width: "40px",
+      height: "40px"
+    }}
+  />
+);
 
-export default Player;
+const mapStateToProps = state => ({
+  ...state.player
+});
+
+export default connect(mapStateToProps)(Player);
