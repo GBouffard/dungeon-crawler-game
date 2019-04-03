48d337a4cefc48fe40f3d23347d44797c16b85aa
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-03 15:50:22 +0200

    Add a map and world where we position both the map and the player

---
+++ src/features/map/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Map = () => (
+  <div
+    style={{
+      position: "relative",
+      top: "0px",
+      left: "0px",
+      width: "800px",
+      height: "400px",
+      backgroundColor: "green",
+      border: "4px solid white"
+    }}
+  />
+);
+
+export default Map;
