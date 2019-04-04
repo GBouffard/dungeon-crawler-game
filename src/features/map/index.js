f3fb62cf5deaf0c9c8148aa19efeefa404948d46
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-04 14:53:57 +0200

    Add some rocks and trees in the map with css and style

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -2,16 +2,27 @@ import React from "react";
 import { SPRITE_SIZE } from "../../config/constants";
 import "./styles.css";
 
+const getTileSprite = type => {
+  switch (type) {
+    case 0:
+      return "grass";
+    case 5:
+      return "rock";
+    case 6:
+      return "tree";
+    default:
+      return "grass";
+  }
+};
+
 const MapTile = ({ tile }) => (
   <div
-    className={`tile ${tile}`}
+    className={`tile ${getTileSprite(tile)}`}
     style={{
       height: SPRITE_SIZE,
       width: SPRITE_SIZE
     }}
-  >
-    {tile}
-  </div>
+  />
 );
 
 const MapRow = ({ tiles }) => (
