2b3b25d5dc279549bff0faf7764e62af47f4794a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-05 16:04:17 +0200

    Updates

---
+++ precache-manifest.161d8cd8e2534d1c2f39aa7b462589be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/dungeon-crawler-game/static/media/player_walk.9f19440f.png"
+  },
+  {
+    "revision": "d272f2e844af0785c880",
+    "url": "/dungeon-crawler-game/static/js/runtime~main.6ccddc3f.js"
+  },
+  {
+    "revision": "ecb3b689c27b3416b78b",
+    "url": "/dungeon-crawler-game/static/js/main.640d2b04.chunk.js"
+  },
+  {
+    "revision": "7aaaf421537f6b65e1d9",
+    "url": "/dungeon-crawler-game/static/js/2.2acd18aa.chunk.js"
+  },
+  {
+    "revision": "ecb3b689c27b3416b78b",
+    "url": "/dungeon-crawler-game/static/css/main.3cb6745d.chunk.css"
+  },
+  {
+    "revision": "946e0f06203b0689b928ec4c80a373b3",
+    "url": "/dungeon-crawler-game/index.html"
+  }
+];
