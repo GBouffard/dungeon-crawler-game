4ca2cc5c3fd3bee39fb2c1d623a25110a0d52b6e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-05 15:42:51 +0200

    Update the manifest

--- src/features/winning-animation/index.js
+++ src/features/winning-animation/index.js
@@ -1,34 +1,34 @@
 const showWinningAnimation = () => {
-  let shouldShowAnimation = false;
-  const body = document.querySelector("body");
+  let shouldShowAnimation = false
+  const body = document.querySelector('body')
 
-  const canvas = document.createElement("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style = `position:absolute;top:0`;
+  const canvas = document.createElement('canvas')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  canvas.style = `position:absolute;top:0`
 
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d')
 
-  const coin = new Image();
-  coin.src = "./coin.png";
+  const coin = new Image()
+  coin.src = './coin.png'
   coin.onload = () => {
-    body.appendChild(canvas);
-    shouldShowAnimation = true;
-    drawloop();
-  };
+    body.appendChild(canvas)
+    shouldShowAnimation = true
+    drawloop()
+  }
 
-  const coins = [];
+  const coins = []
 
   const drawloop = () => {
     if (shouldShowAnimation) {
       // The window.requestAnimationFrame() method tells the browser that you wish to perform an animation
       // The number of callbacks is usually 60 times per second.
-      requestAnimationFrame(drawloop);
+      requestAnimationFrame(drawloop)
     }
 
     // the context.clearRect(startPosX, startPosY, width ,height) clears the specified pixels within a given rectangle.
     // clear the rectangle each time so the coins trail disappear at each frame
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     // the closer to 0 the less coin falling. The opposite is true for 1.
     if (Math.random() < 0.8) {
@@ -38,16 +38,16 @@ const showWinningAnimation = () => {
         moveDownBy: 3,
         size: 0.5 + Math.random(),
         state: Math.random() * 10
-      });
+      })
     }
 
-    let index = coins.length;
+    let index = coins.length
     while (index--) {
-      const { x, y, size, state } = coins[index];
+      const { x, y, size, state } = coins[index]
 
-      coins[index].state = state > 9 ? 0 : state + 0.1; // 10 images in the sprite
-      coins[index].moveDownBy += 0.3;
-      coins[index].y += coins[index].moveDownBy;
+      coins[index].state = state > 9 ? 0 : state + 0.1 // 10 images in the sprite
+      coins[index].moveDownBy += 0.3
+      coins[index].y += coins[index].moveDownBy
 
       // JavaScript syntax:	context.drawImage(
       // img,startxInImg,startyInImg,imgWidth,imgHeight,posX,posY,width,height);
@@ -61,9 +61,9 @@ const showWinningAnimation = () => {
         y,
         44 * size,
         40 * size
-      );
+      )
     }
-  };
-};
+  }
+}
 
-export default showWinningAnimation;
+export default showWinningAnimation
